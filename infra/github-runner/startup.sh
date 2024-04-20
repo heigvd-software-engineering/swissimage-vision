84dc2825d6e0c63ec2f6f2d61cbfbd3f426d642b
Leonard <73912641+leonardcser@users.noreply.github.com>
2024-04-20 17:27:13 +0200

    Remove cml runner

--- infra/github-runner/startup.sh
+++ infra/github-runner/startup.sh
@@ -3,7 +3,7 @@
 # Configure the runner
 ./config.sh --unattended \
     --url https://github.com/heigvd-software-engineering/swissimage-vision \
-    --replace --token $GITHUB_RUNNER_TOKEN
+    --replace --labels $GITHUB_RUNNER_LABELS --token $GITHUB_RUNNER_TOKEN
 
 # Start the runner
 ./run.sh
